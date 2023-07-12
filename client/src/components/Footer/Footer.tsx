17f1c0b7f11d2391d46c11c0c48f8030e2eaad11
home <home@MacBook-Pro-home.local>
2023-07-12 23:08:39 +0300

    Theme responsive footer

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,10 +1,13 @@
 'use client';
 import React from 'react';
 import Container from '../ui/Container';
+import * as colors from '../../static/colors';
 import { ReactComponent as FacebookSVG } from '../../assets/svgs/Facebook2.svg';
 import { ReactComponent as InstagramSVG } from '../../assets/svgs/Instagram.svg';
+import { useTheme } from '../../context/ThemeContext';
 
 export default function Footer() {
+    const { theme } = useTheme();
     return (
         <div className="border-t-2 py-4 border-secondary/10">
             <Container className="flex flex-row justify-between">
@@ -13,10 +16,22 @@ export default function Footer() {
                 </label>
                 <div className="flex flex-row gap-8 items-center cursor-pointer">
                     <a href="https://facebook.com">
-                        <FacebookSVG height={20} />
+                        <FacebookSVG
+                            height={20}
+                            className="transition-all"
+                            fill={
+                                theme == 'dark' ? colors.white : colors.primary
+                            }
+                        />
                     </a>
                     <a href="https://instagram.com">
-                        <InstagramSVG height={24} />
+                        <InstagramSVG
+                            className="transition-all"
+                            height={28}
+                            fill={
+                                theme == 'dark' ? colors.white : colors.primary
+                            }
+                        />
                     </a>
                 </div>
             </Container>
