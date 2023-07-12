74ea19b5b731191e53bc47e482f91bb838f361e2
home <home@MacBook-Pro-home.local>
2023-07-12 23:09:24 +0300

    fixed footer copyright

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -11,9 +11,7 @@ export default function Footer() {
     return (
         <div className="border-t-2 py-4 border-secondary/10">
             <Container className="flex flex-row justify-between">
-                <label className="text-black/50 font-thin">
-                    © All Rights Reserved
-                </label>
+                <label className=" font-thin">© All Rights Reserved</label>
                 <div className="flex flex-row gap-8 items-center cursor-pointer">
                     <a href="https://facebook.com">
                         <FacebookSVG
