ce3e45ac938f3b0919087acd7432f04f9ec8e5c2
home <home@MacBook-Pro-home.local>
2023-07-12 22:22:06 +0300

    Slider component added

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useBooks } from '../../hooks/getBooks';
 import Layout from '../../Layout';
+import Slider from '../../components/ui/Slider';
 
 export default function Home() {
     const { books, loading } = useBooks();
