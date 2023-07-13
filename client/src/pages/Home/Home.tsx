e75c974ba586151e0ee5ceaa9a87bcdd16af1d6e
home <catalinpce@gmail.com>
2023-07-13 23:29:18 +0300

    Added log token button

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -4,6 +4,7 @@ import Layout from '../../Layout';
 import Slider from '../../components/ui/Slider';
 import { useAuth } from '../../context/AuthContext';
 import Container from '../../components/ui/Container';
+import Button from '../../components/ui/Button';
 
 export default function Home() {
     const { loading, user } = useAuth();
@@ -14,9 +15,18 @@ export default function Home() {
         <Layout>
             <Container>
                 {user && (
-                    <label className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
-                        Logged in as {user?.displayName}
-                    </label>
+                    <>
+                        <label className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
+                            Logged in as {user?.displayName}
+                        </label>
+                        <Button
+                            onClick={async () =>
+                                console.log(await user.getIdToken())
+                            }
+                        >
+                            Log Token
+                        </Button>
+                    </>
                 )}
             </Container>
         </Layout>
