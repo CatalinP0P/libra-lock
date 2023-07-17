df2098f311f539d8371bdd87c7615a95f412c586
home <catalinpce@gmail.com>
2023-07-17 17:00:36 +0300

    Hero bug fix 2

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -12,7 +12,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     }, [localStorage.theme]);
 
     return (
-        <div className="flex flex-col min-h-screen bg-white dark:bg-darkMode text-primary dark:text-white">
+        <div className="flex flex-col min-h-screen bg-white dark:bg-darkMode text-primary dark:text-white overflow-x-hidden">
             <Header />
             <div className="flex-1">{children}</div>
             <Footer />
