0e1fd319089d4f9243f59901cba21ff44704b168
home <catalinpce@gmail.com>
2023-07-17 16:47:36 +0300

    Hero changed

--- client/src/pages/Home/Hero.tsx
+++ client/src/pages/Home/Hero.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
-import heroDarkImage from '../../assets/backgrounds/HeroDark.png';
-import heroLightImage from '../../assets/backgrounds/HeroLight.png';
 import Container from '../../components/ui/Container';
 import { useTheme } from '../../context/ThemeContext';
 import { ReactComponent as HeroImage } from '../../assets/svgs/HeroImage.svg';
+
+import heroDarkImage from '../../assets/backgrounds/HeroDark.png';
+import heroLightImage from '../../assets/backgrounds/HeroLight.png';
+
+import darkLeft from '../../assets/backgrounds/DarkLeft.png';
+import darkRight from '../../assets/backgrounds/DarkRight.png';
+
+import whiteLeft from '../../assets/backgrounds/WhiteLeft.png';
+import whiteRight from '../../assets/backgrounds/WhiteRight.png';
 
 export default function Hero() {
     const { theme } = useTheme();
 
     return (
-        <div className="w-full bg-transparent">
-            <img
+        <div className="w-full bg-transparent relative">
+            {/* <img
                 className="absolute left-0 top-0 w-full h-full object-cover select-none pointer-events-none"
                 src={theme == 'dark' ? heroDarkImage : heroLightImage}
+            /> */}
+
+            <img
+                className="absolute left-0 top-0 w-fit max-w-[75%]"
+                src={theme == 'dark' ? darkLeft : darkRight}
+            />
+
+            <img
+                className="absolute bottom-[-10rem] right-[-10rem] w-[30%] min-w-[20rem] max-w-[30rem]"
+                src={theme == 'dark' ? darkRight : whiteRight}
             />
             <Container className="relative flex flex-col gap-4 w-full min-h-[60vh] justify-center pt-44">
                 <div className="flex flex-col lg:flex-row justify-between w-full items-center lg:items-start lg:pt-32 gap-32">
